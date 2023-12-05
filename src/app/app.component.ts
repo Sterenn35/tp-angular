7c6437546fe7bf9c4962371209b876bc549dbaa4
Sterenn <slh35@hotmail.fr>
2023-12-05 16:27:49 +0100

    Fixed Item and List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { MatCardModule } from '@angular/material/card'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent, HomeComponent, MatCardModule],
+  imports: [CommonModule, RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { IData } from '../../IData';
 import { CommonModule } from '@angular/common';
-import { MatCard } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.less'
 })
